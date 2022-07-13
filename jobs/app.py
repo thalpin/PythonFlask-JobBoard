c922eb4eb15d86a0abc5b90f40fe6c76d5bbf671
Tom Halpin <thalpin@vmware.com>
2022-07-13 13:09:14 +0100

    Formatting fix

--- jobs/app.py
+++ jobs/app.py
@@ -13,7 +13,8 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values = (), commit = False, single = False):
+
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit:
@@ -23,11 +24,13 @@ def execute_sql(sql, values = (), commit = False, single = False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
+
 
 @app.route('/')
 @app.route('/jobs')
